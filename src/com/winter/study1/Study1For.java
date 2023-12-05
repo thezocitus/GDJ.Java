468cd1d2109a91dbb484f4a28cad5ba7b3b83096
Zominwoo <thezoticus@gmail.com>
2023-12-05 11:21:19 +0900

    first

---
+++ src/com/winter/study1/Study1For.java
@@ -0,0 +1,22 @@
+package com.winter.study1;
+
+import java.util.Scanner;
+
+public class Study1For {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("정수하나를 입력하세요");
+		
+		int num = sc.nextInt();
+//		입력받은 정수의 약수를 출력하시오
+		for(int i = 1; i<=num; i++) {
+			if(num%i == 0) {
+				System.out.println(i);
+			}
+		}
+		
+	}
+
+}
