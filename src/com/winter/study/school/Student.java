9fc145b16a183e229089c63d3da8afd9d079df51
Zominwoo <thezoticus@gmail.com>
2023-12-11 09:32:38 +0900

    school

---
+++ src/com/winter/study/school/Student.java
@@ -0,0 +1,14 @@
+package com.winter.study.school;
+
+public class Student {
+
+	//맴버 변수 선언  가능(클래스 내에서)
+	//접근지정자 [그외 지정자]	데이터탑입 변수명 
+	
+	String name;
+	int num;
+	double jumsu;
+	
+	//맴버 메서드 선언
+	
+}
