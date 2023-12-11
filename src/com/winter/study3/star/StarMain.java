a57805ebff2e94ec39803adf5f627a5d7d9704e5
Zominwoo <thezoticus@gmail.com>
2023-12-11 10:24:26 +0900

    star

--- src/com/winter/study3/star/StarMain.java
+++ src/com/winter/study3/star/StarMain.java
@@ -1,5 +1,22 @@
 package com.winter.study3.star;
 
 public class StarMain {
+	
+	public static void main(String[] args) {
+		
+		Marine marine = new Marine();
+		
+		marine.hp = 50;
+		marine.move();
 
+		Marine marine2 = new Marine();
+		
+		marine2 = marine;
+		
+		System.out.println(marine2.hp);
+		
+	}
+	
+	
+	
 }
