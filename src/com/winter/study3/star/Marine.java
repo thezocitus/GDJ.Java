a614fc0b1c3da4683619620636d530b4b64599f9
Zominwoo <thezoticus@gmail.com>
2023-12-11 09:29:50 +0900

    Star

---
+++ src/com/winter/study3/star/Marine.java
@@ -0,0 +1,22 @@
+package com.winter.study3.star;
+
+public class Marine {
+
+	// 접근지정자 [그외지정자] 데이터타입 변수명;
+ 	int hp;
+	Gun gun;
+	
+	
+	
+	//이동
+	public void move() {
+		System.out.println("이동하세요");
+	}
+	//공격
+	
+	//멈춤
+	
+	//정찰
+	
+	
+}
