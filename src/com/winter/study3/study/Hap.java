e3ddd1681c7bba0a1666fe66b7382b09591ede08
Zominwoo <thezoticus@gmail.com>
2023-12-11 17:53:48 +0900

    hap

---
+++ src/com/winter/study3/study/Hap.java
@@ -0,0 +1,20 @@
+package com.winter.study3.study;
+
+public class Hap {
+	
+	//메서드 오버로딩
+	//같은 이름의 메서드를 여러개 만드는것
+	//1. 매개변수의 갯수나, 타ㅣㅂ의 조합이 다르면 가능.
+	
+	
+
+	public void h1(int n1, int n2) {
+		int reasult = n1 + n2 ;
+		System.out.println(reasult);
+		
+	}
+	public void h1(int n1, float n2) {
+		
+	}
+	
+}
