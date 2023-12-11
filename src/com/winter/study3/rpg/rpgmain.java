be8eeabd0d62c0b8459311cbf53be2841a99bdb7
Zominwoo <thezoticus@gmail.com>
2023-12-11 09:31:53 +0900

    rpg

---
+++ src/com/winter/study3/rpg/rpgmain.java
@@ -0,0 +1,25 @@
+package com.winter.study3.rpg;
+
+public class rpgmain {
+
+	public static void main(String[] arges) {
+	
+		Magiction Mac = new Magiction();
+		Mac.level = 99;
+		Mac.INT = 255;
+		Mac.STR = 255;
+		Mac.AGL = 255;
+		
+	
+	}
+
+	
+	//	String id;
+//	int level;
+//	int INT;
+//	int STR;
+//	int AGL;
+//	String skill;
+	
+	
+}
