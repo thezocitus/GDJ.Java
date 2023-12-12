a4e30381b124ac8f4336416f442ca9e48fcb25a5
Zominwoo <thezoticus@gmail.com>
2023-12-12 09:36:39 +0900

    School

--- src/com/winter/study/school/SchoolService.java
+++ src/com/winter/study/school/SchoolService.java
@@ -3,17 +3,17 @@ package com.winter.study.school;
 import java.util.Scanner; 
 
 public class SchoolService {
+	
+	Scanner sc = new Scanner(System.in);
 
 	//addstudent
 	//이름, 번호, 점수를 입력받아 기존배열에 추가.
 	//findByNum
 	//학생번호를 입력받아, 일치하는 학생을 검색
 	//해당학생을 리턴 없으면 null
-	public Student[] addstudent(Student[] students) {
-		Scanner sc = new Scanner(System.in);
-		int num = students.length+1;
-		Student [] temp = new Student[num];		
-		for(int i = 0 ; i<num;i++) {
+	public Student[] addstudent(Student[] students) {		
+		Student [] temp = new Student[students.length+1];		
+		for(int i = 0 ; i<=students.length;i++) {
 			
 			if(i<students.length){				
 				temp[i]=students[i];
@@ -25,7 +25,7 @@ public class SchoolService {
 				System.out.println(i+1+"번 학생 번호를 입력하시오");
 				student.num = sc.nextInt();
 				System.out.println(i+1+"번 학생 점수를 입력하시오");
-				student.jumsu = sc.nextInt();
+				student.jumsu = sc.nextDouble();
 				temp[i] = student;
 				
 			}
@@ -36,8 +36,7 @@ public class SchoolService {
 	}
 	
 	
-	public Student findByNum(Student[] students) {
-		Scanner sc = new Scanner(System.in);
+	public Student findByNum(Student[] students) {	
 		System.out.println("검색할 학생 번호를 입력하시오");
 		int num = sc.nextInt();	
 		Student student = null;		
@@ -59,8 +58,7 @@ public class SchoolService {
 	
 	//메서드 amkeStudent
 	//학생수를 입력 받아서 수만큼 학생정보를 입력
-	public Student [] makeStudents() {		
-		Scanner sc = new Scanner(System.in);		
+	public Student [] makeStudents() {					
 		System.out.println("학생수를 입력하시오.");
 		int count =sc.nextInt();		
 		
