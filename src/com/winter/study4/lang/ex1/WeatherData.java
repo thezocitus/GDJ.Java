ba00c86159e2b2646b9458294d1891c77b9792ff
Zominwoo <thezoticus@gmail.com>
2023-12-14 18:20:44 +0900

    ex1

---
+++ src/com/winter/study4/lang/ex1/WeatherData.java
@@ -0,0 +1,38 @@
+package com.winter.study4.lang.ex1;
+
+public class WeatherData {
+
+	
+	private String city;
+	private String gion;
+	private String info;
+	private String hum;
+	
+	
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getGion() {
+		return gion;
+	}
+	public void setGion(String gion) {
+		this.gion = gion;
+	}
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	public String getHum() {
+		return hum;
+	}
+	public void setHum(String hum) {
+		this.hum = hum;
+	}
+	
+	
+}
