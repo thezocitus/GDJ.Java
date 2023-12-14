f4f3429e50119be59603776c5024fd54a589c274
Zominwoo <thezoticus@gmail.com>
2023-12-14 18:21:10 +0900

    study4

---
+++ src/com/winter/study4/ApiMain.java
@@ -0,0 +1,29 @@
+package com.winter.study4;
+
+public class ApiMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println(String.CASE_INSENSITIVE_ORDER);
+		
+		
+		Object obj = new Object();
+		
+		
+		
+//		주소를 담고 있는 변수;
+		
+//		참조변수명.변수명
+//		참조변수명.메서드명()
+		
+//	 	boolean ch = obj.equals(obj);
+//	 	
+//	 	System.out.println(ch);
+//	 	
+//	 	String h =obj.toString();
+//	 	
+//	 	System.out.println(h);	
+		
+	}
+	
+}
---
+++ src/com/winter/study4/Test.java
@@ -0,0 +1,12 @@
+package com.winter.study4;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+
+}
