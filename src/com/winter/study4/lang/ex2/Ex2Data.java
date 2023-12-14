ce7a8f6defab14319566bc723d6835a39b8cb39f
Zominwoo <thezoticus@gmail.com>
2023-12-14 18:20:50 +0900

    ex2

---
+++ src/com/winter/study4/lang/ex2/Ex2Data.java
@@ -0,0 +1,8 @@
+package com.winter.study4.lang.ex2;
+
+public class Ex2Data {
+
+	String minbun;
+	
+	
+}
---
+++ src/com/winter/study4/lang/ex2/Ex2Main.java
@@ -0,0 +1,33 @@
+package com.winter.study4.lang.ex2;
+
+import java.util.Scanner;
+
+public class Ex2Main {
+
+	public static void main(String[] args) {
+				
+		// TODO Auto-generated method stub
+	
+		Ex2Controller ex2Controller = new Ex2Controller();
+		ex2Controller.start();
+			
+		
+	
+		//1. 남녀구분
+		//2. 나이 계산
+		//3. 태어난 계절이 뭐냐 봄-345 여름-678
+		//4. 주민번호 타당
+		//	 9 9 1 2 2 5 - 1 2 3 4 5 6 7
+		//	 2 3 4 5 6 7   8 9 2 3 4 5 
+		// 총합구하기
+		// ex)122(총합)을 11로 나눈 나머지를 구함	
+		// 11에서 나머지를 뺌 값 
+		// 만약 나머지가 2자리
+		// 그 나머지를 10으로 나눈 나머지를 구함
+		// 다시 나머지 와 마지막 자리 비교
+		// 주민번호 마지막 자리와 비교, 다르면 false
+		
+		
+	}
+
+}
