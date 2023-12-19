32f717f28e09640d9a465ee1c66ffb43baef1af8
Zominwoo <thezoticus@gmail.com>
2023-12-19 20:02:07 +0900

    1

--- src/com/winter/study5/Study5Main.java
+++ src/com/winter/study5/Study5Main.java
@@ -4,42 +4,51 @@ import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-
-
 public class Study5Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("숫자 입력");
-		try {
-			int num = sc.nextInt();
-			throw new InputMismatchException();
-			
-		
-		System.out.println("정상진행");
-		String str=null;
-		
-		
 		Test t = new Test();
+		try {
+			int num=sc.nextInt();
+			//throw new InputMismatchException();
 		
-		int [] arr = new int[2];
-		arr[3]=10;
+			System.out.println("정상 진행");
+			String str=null;
+//		Error		
+//		int a;
+//		System.out.println(a);
+		
+			
+			int [] arr = new int[2];
+			
+			arr[3]=10;
+		//new ArrayIndexOutOfBoundsException();
+		}catch (InputMismatchException e) {
+			
+			// TODO: handle exception
+		}catch (ArrayIndexOutOfBoundsException e) {
+			// TODO: handle exception
+			
 		}catch (RuntimeException e) {
-			System.out.println("Exception 발생");
+			// TODO: handle exception
+		}catch (Exception e) {
+			// TODO: handle exception
+		}catch (Throwable e) {
+			// TODO: handle exception
+		}finally {
+			
 		}
 		
-		int a = 10;
+		int a =10;
+		a = a/0;
+		new ArithmeticException();
 		
-		a= a/0;		
-		
-		
-		ArrayList<Integer> ar = new ArrayList<>();
+		ArrayList<Integer> ar=null;
 		t.t1(ar);
-		
-		
-//		System.out.println(str.toString());
-		
+
 		System.out.println("종료 합니다.");
 	}
-	
+
 }
